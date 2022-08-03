80dfb32b6cdad7b400184caa4cce3ea32255cff2
Justin Hurstwright <justinhurstwright@Justin.home>
2022-08-03 12:52:07 -0500

    init project

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as Server from 'react-dom/server'
+
+const App = (props: { message: string }) => {
+    const [count, setCount] = React.useState(0);
+    const increment = React.useCallback(() => {
+        setCount(count => count + 1);
+    }, [count]);
+    return(<>
+        <h1>{props.message}</h1>
+        <h2>Count: {count}</h2>
+        <button onClick={increment}>Increment</button>
+    </>)
+};
+
+ReactDOM.render(<App message="Hello World" />, document.getElementById('root'));
